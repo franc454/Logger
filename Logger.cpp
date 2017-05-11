2fdeff3e805de17a04041c7d3fc7e4c09b5c6c71
franc454 <franc454@umn.edu>
2017-05-11 16:10:03 -0500

    Update Logger.cpp

--- Logger.cpp
+++ Logger.cpp
@@ -1704,10 +1704,6 @@ void Logger::HM1500LF_humidity_with_external_temperature(int humidPin, \
 // MaxBotix ruggedized standard size ultrasonic rangefinder: 
 // 1 cm = 1 10-bit ADC interval
 //////////////////////////////////////////////////////////////
-char Logger::GPSdata(char c){
-	datafile.print(c);
-	Serial.print(c);
-}
 
 void Logger::ultrasonicMB_analog_1cm(int nping, int Ex, int sonicPin, bool writeAll){
 
@@ -3358,6 +3354,76 @@ float Logger::Honeywell_HSC_analog(int pin, float Vsupply, float Vref, \
 
 }
 
+void Logger::UltimateGPS(){
+	  // connect at 115200 so we can read the GPS fast enuf and
+  // also spit it out
+  Serial.begin(115200);
+  Serial.println("Adafruit GPS library basic test!");
+
+  // 9600 NMEA is the default baud rate for MTK - some use 4800
+  GPS.begin(9600);
+  
+  // You can adjust which sentences to have the module emit, below
+  
+  // uncomment this line to turn on RMC (recommended minimum) and GGA (fix data) including altitude
+  //GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
+  // uncomment this line to turn on only the "minimum recommended" data for high update rates!
+  //GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCONLY);
+  // uncomment this line to turn on all the available data - for 9600 baud you'll want 1 Hz rate
+  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_ALLDATA);
+  
+  // Set the update rate
+  // Note you must send both commands below to change both the output rate (how often the position
+  // is written to the serial line), and the position fix rate.
+  // 1 Hz update rate
+  //GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);
+  //GPS.sendCommand(PMTK_API_SET_FIX_CTL_1HZ);
+  // 5 Hz update rate- for 9600 baud you'll have to set the output to RMC or RMCGGA only (see above)
+  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_5HZ);
+  GPS.sendCommand(PMTK_API_SET_FIX_CTL_5HZ);
+  // 10 Hz update rate - for 9600 baud you'll have to set the output to RMC only (see above)
+  // Note the position can only be updated at most 5 times a second so it will lag behind serial output.
+  //GPS.sendCommand(PMTK_SET_NMEA_UPDATE_10HZ);
+  //GPS.sendCommand(PMTK_API_SET_FIX_CTL_5HZ);
+
+  // Request updates on antenna status, comment out to keep quiet
+  GPS.sendCommand(PGCMD_ANTENNA);
+
+  // the nice thing about this code is you can have a timer0 interrupt go off
+  // every 1 millisecond, and read data from the GPS for you. that makes the
+  // loop code a heck of a lot easier!
+  useInterrupt(true);
+  
+  delay(1000);
+
+
+// Interrupt is called once a millisecond, looks for any new GPS data, and stores it
+SIGNAL(TIMER0_COMPA_vect) {
+  char c = GPS.read();
+  datafile.println(c)
+  // if you want to debug, this is a good time to do it!
+  if (GPSECHO)
+    if (c) UDR0 = c;  
+    // writing direct to UDR0 is much much faster than Serial.print 
+    // but only one character can be written at a time. 
+}
+
+
+void useInterrupt(boolean v) {
+  if (v) {
+    // Timer0 is already used for millis() - we'll just interrupt somewhere
+    // in the middle and call the "Compare A" function above
+    OCR0A = 0xAF;
+    TIMSK0 |= _BV(OCIE0A);
+    usingInterrupt = true;
+  } else {
+    // do not call the interrupt function COMPA anymore
+    TIMSK0 &= ~_BV(OCIE0A);
+    usingInterrupt = false;
+  }
+}
+}
+
 void Logger::vdivR(int pin, float Rref, uint8_t ADC_resolution_nbits, bool Rref_on_GND_side){
   /**
    * @brief
@@ -3406,6 +3472,8 @@ void Logger::vdivR(int pin, float Rref, uint8_t ADC_resolution_nbits, bool Rref_
   Serial.print(F(","));
 
 }
+
+
 
 /*
 SENSOR DOES NOT STABILIZE (FLEXFORCE SENSOR)
